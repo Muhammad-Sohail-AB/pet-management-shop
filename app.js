bf6282dbf457e6da6c1ec92c7371ea8b6a997a1a
Muhammad Sohail <msohailshaheen5@gmail.com>
2023-04-29 18:56:52 +0500

    added docgen docs

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const app = express();
 // other packages
 const morgan = require('morgan');
 const expressFileUpload = require('express-fileupload');
-
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 // data base
 const connectDB = require('./db/connect');
 // routers
@@ -18,10 +22,21 @@ const horseRouter = require('./routes/horseRouter');
 const errorHandler = require('./middleWare/error-handler');
 const notFound = require('./middleWare/not-found');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(express.static('./public'));
 app.use(express.json());
 app.use(expressFileUpload());
-app.use(morgan('tiny'));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
